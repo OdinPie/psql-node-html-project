966708c3460414a0834547c1e5e7389614b85505
Maliha <maliha16tabassum@gmail.com>
2024-09-22 22:42:04 +0600

    added comment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname)); //middleware
 const {Client} = require('pg');
 
 //initiating connection
